23178f3724e949f58e809da402e837f4c33da917
princenitk <princekumarsinha321@gmail.com>
2023-11-03 15:30:26 +0530

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # this is a simple webpage
+
+
+<!-- create table student(
+    id int AUTO_INCREMENT primary key,
+    username varchar(255),
+    roll int,
+    email varchar(100)
+
+); -->
---
+++ app.js
@@ -0,0 +1,103 @@
+
+num = 5;
+str = "10";
+let newString = str + num;
+const intNumber = Number(newString);
+console.log(typeof intNumber);
+console.log(newString);
+
+function fetchData(callback) {
+
+    setTimeout(function() {
+      const data = 'Some fetched data';
+      callback(data); 
+    }, 3000); 
+  }
+  
+  function processFetchedData(data) {
+    console.log('Processing data:', data);
+  }
+
+  fetchData(processFetchedData);
+  
+
+
+
+strNumber = "df123abc";
+n = parseInt(strNumber);
+console.log(n);
+
+const person = {
+    firstName : "Prince",
+    lastName : "Kumar",
+    age : 23,
+    Course : "Mca",
+
+    greet : function(){
+        console.log(`Hello my name is ${this.firstName}, thank you for visiting me`);
+    }
+}
+
+console.log(person.firstName);
+person.greet();
+
+
+
+
+// Promises in javascript
+
+let promise = new Promise((res, err)=>{
+    let a = 2 + 5;
+    if(a == 7){
+        res("promise resolved");
+    }
+    else err("Promise rejected");
+})
+
+promise.then((msg)=>{
+    console.log("This is then method, " + msg);
+}).catch((err)=>{
+    console.log("This is in catch block, " + err);
+})
+
+
+// Async Await
+
+const connect = ()=>{
+    return "Connected";
+}
+
+function connectDatabase(){
+    try{
+        let msg = "Connecting to database";
+        setTimeout(async()=>{
+            var conn = await connect();
+            console.log( msg + conn);
+        }, 3000)
+
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+connectDatabase();
+
+// Closure in js. Basically closure allows a function to access member from a
+// function which has already been executed.
+
+ 
+const sum = function(a){
+    var c = 6;
+
+    return function(b){
+        return a + b + c;
+    }
+}
+
+const store = sum(5);
+console.log(store(10));   // here we can access values of a and c variables
+
+
+const arr = [1,2,3];
+console.log(arr);
